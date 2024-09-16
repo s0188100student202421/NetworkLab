fb1e86c402c0f6dfb47d6e3c7ac149a8ac0db6bb
s0188100student202421 <uliamaslaeva88@gmail.com>
2024-09-16 20:31:56 +0300

    abb first lab

---
+++ Network_lab1/Client.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+// структура данных запроса клиента
+struct Person
+{
+    char name[25]; //имя
+    int faculty;//факультет
+    int informatica; //баллы инф
+    int math; // баллы мат
+    int russ;// баллы рус
+} Student;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char* nameR = "request.bin";
+//файл запросов клиентов
+    char* nameA = "answers.bin";
+//файл для ответов сервера
+    ofstream f_REQ;
+    ifstream f_ANS;
+    long pred_size;
+    int answer;
+    while (true)
+    {
+// передача данных от клиента серверу
+        cout << "Введите запрос: Фамилия студента   Номер направления  Баллы Информатика   Баллы математика    Баллы Русский язык"<<endl;
+        cin >> Student.name >> Student.faculty  >> Student.informatica >> Student.math >> Student.russ;
+        if (!isdigit(Student.faculty) && Student.faculty != 10302 && Student.faculty != 20302 && Student.faculty != 20303 && Student.faculty != 90303){
+            cout << "Введите корректный номер направления (010302 - пми , 020302 - пи, 020303 - фундам, 090303 - матобес ): ";
+            cin >> Student.faculty;
+        }
+        cout << Student.name <<"\t"<< "0" <<Student.faculty <<"\t"<< Student.informatica <<"\t"<< Student.math <<"\t"<< Student.russ;
+        f_REQ.open( nameR, ios::app |ios::binary);
+//открытие файла REQUEST
+        f_REQ.write((char*)&Student, sizeof(Student));
+//запись запроса в файл REQUEST
+        f_REQ.close();
+// поступил ответ от сервера?
+        f_ANS.open(nameA, ios::binary);
+//открытие файла ANSWER
+        f_ANS.seekg(0, ios::end);
+//переход в конец файла ANSWER
+        pred_size = f_ANS.tellg();
+        while (pred_size >= f_ANS.tellg())
+        {
+            sleep(100);
+// ждем и переходим в конец файла ANSWER
+            f_ANS.seekg(0, ios::end);}
+// получение ответа от сервера
+        f_ANS.seekg(pred_size, ios::beg);
+// на начало нового ответа
+        f_ANS.read ((char*)&answer, sizeof(answer)) ;
+//считывание ответа
+        f_ANS.close();
+// расшифровка ответа
+        switch (answer) {
+            case 0: {cout <<"   Сожалеем, но вы не проходите на это направление :) \n";break;}
+            case 1: {cout << "  Поздравляем! Вы проходите на введеное вами направление"<< "\n";break;}
+        }
+    }
+}
---
+++ Network_lab1/Server.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+
+using namespace std;
+//
+struct Person
+{
+    char name[25]; //имя
+    int faculty;//факультет
+    int informatica; //баллы инф
+    int math; // баллы мат
+    int russ;// баллы рус
+}Student;
+
+int answer;
+long size_pred;
+int main()
+{
+    ifstream fR;
+    ofstream fA;
+    setlocale(LC_ALL, "rus");
+    const string nameR = "request.bin";
+//файл запросов клиентов
+    string nameA = "answers.bin";
+//файл ответов сервера
+    cout<< "server is working"<< endl;
+// начальные установки
+    fR.open(nameR, ios::binary);
+//открытие файла REQUEST
+    fR.seekg(0, ios::end);
+    size_pred = fR.tellg();
+//стартовая позиция сервера в файле REQUEST
+    fR.close();
+    while (true)
+    {
+        fR.open(nameR, ios::binary);
+        fR.seekg(0, ios::end);
+//переход в конец файла REQUEST
+// есть новые запросы от клиентов? как только fR.tellg() станет больше переходим к след.операции
+        while (size_pred >= fR.tellg())
+        {
+            sleep(100);
+            fR.seekg(0, ios::end);}
+// получен новый запрос
+        fR.seekg(size_pred, ios::beg);
+//переход к началу полученного запроса
+        fR.read((char*)&Student, sizeof(Student));
+//считывание данных клиента
+        size_pred = fR.tellg();
+// на конец обработанных данных
+        fR.close();
+// определение баллов
+        int points = Student.informatica + Student.math + Student.russ;
+        switch(Student.faculty) {
+            case 90303:
+                answer = points >= 238 ? 1 : 0;
+            case 20303:
+                answer = points >= 237 ? 1 : 0;
+            case 20302:
+                answer = points >= 245 ? 1 : 0;
+            case 10302:
+                answer = points >= 233 ? 1 : 0;
+
+        }
+// передача ответа клиенту
+        fA.open(nameA, ios::binary | ios::app);
+//открытие файла ANSWER
+        fA.write( (char*)&answer, sizeof(answer));
+//запись ответа клиенту
+        fA.close();
+    }
+}
